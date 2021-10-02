c32a8bef0708c827917dbc109b4f3f0efcfbd5ff
XixiSun <sunxixi1995@163.com>
2021-10-02 18:08:42 +0800

    Update ftp_himawari8_hsd.py
    add documentation for functions

--- ftp_himawari8_hsd/ftp_himawari8_hsd.py
+++ ftp_himawari8_hsd/ftp_himawari8_hsd.py
@@ -52,6 +52,23 @@ from pandas.core.common import flatten
 # when a file is downloaded its size is also displayed to the user along with the filename.
 
 def downloadfiles(download_path: Path, start_date: str, end_date: str):
+    """
+    Save specified time range files to specified file folder.
+       
+    Parameters
+    ----------
+    download_path : Path
+                 A file folder to save downloaded files
+    start_date : str
+              Start date time for downloaed files
+    end_date : str
+            End date time for downloaed files
+               
+    Returns
+    -------
+    Himawari Standard Data in DAT format
+
+    """
     try:
         # format user input to date format
         sdate = dateparser.parse(start_date)
@@ -183,6 +200,7 @@ def downloadfiles(download_path: Path, start_date: str, end_date: str):
 
 
 def download():
+    """Execute downloading."""
     start_date = input("Enter start datetime yyyy/mm/dd hh:mm: ")
     print(start_date)
 
